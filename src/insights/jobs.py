acca3327022d23f851290892ffb1041041eccd09
eric-nas <ericnascimentocruzc@gmail.com>
2024-01-29 14:44:05 -0300

    req3 e 4

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -12,11 +12,21 @@ class ProcessJobs:
 
             for row in reader:
                 self.jobs_list.append(row)
-        
+
         return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
-        pass
+        job_types = set()
+        for jobs in self.jobs_list:
+            job_types.add(jobs['job_type'])
+        return list(job_types)
 
-    def filter_by_multiple_criteria(self) -> List[dict]:
-        pass
+
+
+    def filter_by_multiple_criteria(
+            self, jobs: List[Dict], criteria: Dict) -> List[dict]:
+        filter_jobs = list()
+        for job in jobs:
+            if job['industry'] == criteria['industry'] or job['job_type'] == criteria['job_type']:
+                filter_jobs.append(job)
+        return filter_jobs
