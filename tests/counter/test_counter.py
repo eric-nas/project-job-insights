c630b5ee38d7600226334ad35aaa66e9f6e57c7c
eric-nas <ericnascimentocruzc@gmail.com>
2024-01-29 12:06:57 -0300

    Req1 e 2

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    raise
+    path = "data/jobs.csv"
+
+    assert count_ocurrences(path, 'python') == 1639
+    assert count_ocurrences(path, 'javascript') == 122
